6d7e84fa2792159434ec5f8492f1762c06662453
Dutraz <53490994+Dutraz@users.noreply.github.com>
2022-11-15 16:30:51 -0300

    Pouplando a tabela com os metadados

---
+++ src/init.py
@@ -0,0 +1,50 @@
+import json
+from db import exec
+
+
+def createTable():
+    exec('''
+        CREATE TABLE log (
+            id SERIAL PRIMARY KEY,
+            a integer NOT NULL,
+            b integer NOT NULL
+        )
+        ''')
+
+
+def dropTable():
+    exec('DROP TABLE log')
+
+
+def insert(a, b):
+    exec(f'INSERT INTO log (a, b) VALUES ({a}, {b})')
+
+
+# Lê o arquivo que contém os valores iniciais do banco
+def getMetadados():
+    data = None
+    try:
+        f = open('../files/metadado.json')
+        data = json.load(f)['INITIAL']
+        f.close()
+    except:
+        print('Erro ao abrir arquivo de metadados.')
+        exit()
+    return list(zip(data['A'], data['B']))
+
+
+# Popula a tabela com os valores do arquivo de metadados
+def populateTable():
+    data = getMetadados()
+    for d in data:
+        insert(d[0], d[1])
+
+
+def init():
+    dropTable()
+    createTable()
+    populateTable()
+
+
+if __name__ == '__main__':
+    init()
---
+++ src/redo.py
@@ -0,0 +1,10 @@
+from db import exec
+from init import init
+
+
+def redo():
+    init()
+
+
+if __name__ == '__main__':
+    redo()
